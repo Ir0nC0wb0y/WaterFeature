67323cb2a144ccdfa7678982fbcc71979a3664e0
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2019-09-05 00:00:49 -0500

    Update main.cpp
    added important movement logic

--- WaterFeature/src/main.cpp
+++ WaterFeature/src/main.cpp
@@ -4,18 +4,18 @@
 
 // Variables needed by CheapStepper
   CheapStepper pump_stepper(8,9,10,11);
-  bool pump_stepper_dir   =    true;
-  #define STEPS_ROT            4076
+  bool pump_stepper_dir        =          true;
+  #define STEPS_ROT                       4076
 
 // Variables for timing
-  int drip_qty            =      2;              // [drips]
-  unsigned long time_next_drip = 0;              // number of millis until next drip
+  int drip_qty                 =             2;              // [drips]
+  unsigned long time_next_drip =             0;              // number of millis until next drip
   unsigned long time_last_drip;
-  #define DRIP_QTY_MIN           1               // [drips]
-  #define DRIP_QTY_MAX          20               // [drips]
-  #define DRIP_PERIOD_MIN     5000               // [millis]
-  #define DRIP_PERIOD_MAX    30000               // [millis]
-  #define STEPS_PER_DRIP       590               // This is a guess for now
+  #define DRIP_QTY_MIN                       1               // [drips]
+  #define DRIP_QTY_MAX                      20               // [drips]
+  #define DRIP_PERIOD_MIN   DRIP_QTY_MAX * 722               // [millis] or ~722ms per drop
+  #define DRIP_PERIOD_MAX                90000               // [millis]
+  #define STEPS_PER_DRIP                   590               // This is a guess for now
         // steps_turn = (4 * steps_rot * Vd) / (pi^2 * ID_tube^2 * OD_pump)
         // Where:
         //  ID_tube   =  2 [mm]
@@ -24,6 +24,7 @@
         //
         // Gives:
         //  steps_turn = 590 (rounded up)
+        //  angle_turn ~ 52 [degrees]
 
 
 
@@ -34,6 +35,7 @@ void setup() {
   Serial.begin(9600);
 
   // Lets do a drip or 5 to start:
+  Serial.println("Pushing 5 drops");
   pump_stepper.newMove(pump_stepper_dir, drip_qty * STEPS_PER_DRIP);
   time_last_drip = millis();
   
@@ -45,14 +47,18 @@ void loop() {
   // if we are using non-blocking moves
   pump_stepper.run();
 
-  // when there are no more steps, and the wait time has elapsed,
+  // when there are no more steps, and the wait time has elapsed, do the drops
   int stepsLeft = pump_stepper.getStepsLeft();
   bool check_steps = stepsLeft == 0;
-  bool check_time = millis() - time_last_drip >= time_next_drip;
-  if ( check_steps && check_time ) {
-    time_next_drip = random(DRIP_PERIOD_MIN, DRIP_PERIOD_MAX);
-    drip_qty       = random(   DRIP_QTY_MIN,    DRIP_QTY_MAX);
-    Serial.print("Next drop sequence: (time_next_drip, drip_qty): "); Serial.print(time_next_drip); Serial.print(", "); Serial.println(drip_qty);
+  if ( check_steps ) {
+    bool check_time = millis() - time_last_drip >= time_next_drip;
+    if ( check_time ) {
+      time_next_drip = random(DRIP_PERIOD_MIN, DRIP_PERIOD_MAX);
+      drip_qty       = random(   DRIP_QTY_MIN,    DRIP_QTY_MAX);
+      Serial.print("Next drop sequence: (time_next_drip, drip_qty): "); Serial.print(time_next_drip); Serial.print(", "); Serial.println(drip_qty);
+      pump_stepper.newMove(pump_stepper_dir, drip_qty * STEPS_PER_DRIP);
+      time_last_drip = millis();
+    }
   }
 
 }
